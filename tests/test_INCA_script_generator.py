7b9f472e462f6b3cb59b074d7090070c96c49577
mmattano <57752712+mmattano@users.noreply.github.com>
2020-12-02 11:11:31 +0100

    fix: adjusted using black and quick fix for line length issues
    Added "# noqa E501" to lines that were to long where I could not just break them (the strings that are necessary for setting up the MATLAB file) because breaking them caused issues with the resulting script

--- tests/test_INCA_script_generator.py
+++ tests/test_INCA_script_generator.py
@@ -2,20 +2,31 @@ import unittest
 import pickle
 import sys
 import pathlib
-sys.path.insert(1, '../')
-from AutoFlow_OmicsDataHandling.INCA_script_generator import *
+
+sys.path.insert(1, "../")
+from AutoFlow_OmicsDataHandling.INCA_script_generator import ( # noqa E402
+    script_generator,
+    runner_script_generator,
+)
 
 current_dir = str(pathlib.Path(__file__).parent.absolute())
 
 
 class test_methods(unittest.TestCase):
-
     def setUp(self):
-        file_obj = open(current_dir + '/test_data/MFA_modelInputsData/test_data.obj', 'rb')
-        modelReaction_data_I, atomMappingReactions_data_I, \
-            atomMappingMetabolite_data_I, measuredFluxes_data_I, \
-            experimentalMS_data_I, tracer_I, script, \
-            runner = pickle.load(file_obj)
+        file_obj = open(
+            current_dir + "/test_data/MFA_modelInputsData/test_data.obj", "rb"
+        )
+        (
+            modelReaction_data_I,
+            atomMappingReactions_data_I,
+            atomMappingMetabolite_data_I,
+            measuredFluxes_data_I,
+            experimentalMS_data_I,
+            tracer_I,
+            script,
+            runner,
+        ) = pickle.load(file_obj)
         file_obj.close()
 
         self.modelReaction_data_I = modelReaction_data_I
@@ -28,16 +39,18 @@ class test_methods(unittest.TestCase):
         self.runner = runner
 
     def test_script_generator(self):
-        script_ = script_generator(self.modelReaction_data_I,
-                                   self.atomMappingReactions_data_I,
-                                   self.atomMappingMetabolite_data_I,
-                                   self.measuredFluxes_data_I,
-                                   self.experimentalMS_data_I, self.tracer_I)
+        script_ = script_generator(
+            self.modelReaction_data_I,
+            self.atomMappingReactions_data_I,
+            self.atomMappingMetabolite_data_I,
+            self.measuredFluxes_data_I,
+            self.experimentalMS_data_I,
+            self.tracer_I,
+        )
         script = self.script
         self.assertEqual(script, script_)
 
     def test_runner_generator(self):
-        runner_ = runner_script_generator('TestFile', n_estimates=10)
+        runner_ = runner_script_generator("TestFile", n_estimates=10)
         runner = self.runner
         self.assertEqual(runner, runner_)
-        
