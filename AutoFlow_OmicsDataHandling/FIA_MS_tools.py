54228f8c5ac3122b641ee73dd62ed7f4795eb4e2
mmattano <57752712+mmattano@users.noreply.github.com>
2020-12-11 10:41:59 +0100

    fix: deleted unused packages

--- AutoFlow_OmicsDataHandling/FIA_MS_tools.py
+++ AutoFlow_OmicsDataHandling/FIA_MS_tools.py
@@ -1,13 +1,6 @@
 import numpy as np
 import pandas as pd
-
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 from pyopenms import FeatureMap, FeatureXMLFile
-
-# import copy
-# import re
-# import math
 
 
 def extractNamesAndIntensities(feature_dir, sample_names, database):
