6221045a8f9b19d8332b72771535c01b5f0e22fd
mmattano <57752712+mmattano@users.noreply.github.com>
2020-12-13 16:27:36 +0100

    tests: changed test data generation
    Also generated it newly

--- tests/test_data/FIA_MS_Data/unittest_data/generate_FIA_MS_test_data.py
+++ tests/test_data/FIA_MS_Data/unittest_data/generate_FIA_MS_test_data.py
@@ -1,37 +1,40 @@
 # generate test_data
-# Last date : 11.12.2020
+# Last date : 13.12.2020
 # By : Matthias Mattanovich (matmat@biosustain.dtu.dk)
 # This script is intended to generate sample data and save them into the
 # test_data file. The saved objects will then be used to test the
 # tecan_od_analyzer using unit testing.
 import pickle
 import pandas as pd
+import pathlib
 from AutoFlow_OmicsDataHandling.FIA_MS_tools import (
     extractNamesAndIntensities,
     calculateMeanVarRSD,
 )
 
+current_dir = str(pathlib.Path(__file__).parent.absolute())
 
 pd.set_option("mode.chained_assignment", None)
 # Use pickle to save python variables
 filehandler = open("test_data.obj", "wb")
 
 # load data and assign variables needed for the functions
-feature_dir = "../features_AdditionalAdducts"
-sequence_triplicates = pd.read_csv("../sequence_EColi.csv")
+feature_dir = current_dir + "/../features_AdditionalAdducts"
+sequence_triplicates = pd.read_csv(current_dir + "/../sequence_EColi.csv", sep=";")
+print(sequence_triplicates.columns.values)
 sample_names_triplicates = sequence_triplicates["sample_group_name"].unique()
 database_triplicates = pd.read_csv(
-    "../CHEMISTRY/iJO1366_struct.tsv", sep="\t", header=None
+    current_dir + "/../CHEMISTRY/iJO1366_struct.tsv", sep="\t", header=None
 )
-sequence_single = pd.read_csv("../sequence_HumanSerum.csv")
+sequence_single = pd.read_csv(current_dir + "/../sequence_HumanSerum.csv", sep=";")
 sample_names_single = sequence_single["sample_group_name"].unique()
 database_single = pd.read_csv(
-    "../CHEMISTRY/HMDB_struct.tsv", sep="\t", header=None
+    current_dir + "/../CHEMISTRY/HMDB_struct.tsv", sep="\t", header=None
 )
-sequence_standard = pd.read_csv("../sequence_Standards.csv")
+sequence_standard = pd.read_csv(current_dir + "/../sequence_Standards.csv", sep=";")
 sample_names_standard = sequence_standard["sample_group_name"].unique()
 database_standard = pd.read_csv(
-    "../CHEMISTRY/standard_mix_struct.tsv", sep="\t", header=None
+    current_dir + "/../CHEMISTRY/standard_mix_struct.tsv", sep="\t", header=None
 )
 
 
