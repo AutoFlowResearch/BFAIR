209cfdd48da8a96ef592e5110f2e7d9d48a054f2
mmattano <57752712+mmattano@users.noreply.github.com>
2021-01-11 22:13:55 +0100

    feat: FIA_MS_tools

---
+++ AutoFlow_OmicsDataHandling/FIA_MS/FIA_MS_tools.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+from pyopenms import FeatureMap, FeatureXMLFile
+
+__version__ = "0.0.1"
+
+
+def extractNamesAndIntensities(feature_dir, sample_names, database):
+    metabolites_unique = database[0].unique()
+    extracted_data_dict = {}
+    cnt = 0
+    for name in sample_names:
+        features = FeatureMap()
+        FeatureXMLFile().load(
+            feature_dir + "/" + name + ".featureXML", features
+        )
+        for f in features:
+            try:
+                peptideRef = f.getMetaValue("PeptideRef").decode("utf-8")
+            except AttributeError:
+                peptideRef = f.getMetaValue("PeptideRef")
+            if (
+                peptideRef
+                in metabolites_unique
+            ):
+                formula = database[
+                    database[0] == peptideRef
+                ][1]
+                extracted_data_dict[cnt] = {
+                    "sample_group_name": name,
+                    "Metabolite": peptideRef,
+                    "Formula": list(formula)[0],
+                    "Intensity": f.getMetaValue("peak_apex_int"),
+                }
+                cnt = cnt + 1
+    return pd.DataFrame.from_dict(extracted_data_dict, "index")
+
+
+def calculateMeanVarRSD(
+    extracted_data_all, sample_name_2_replicate_groups, min_reps=3
+):
+    stats_all_dict = {}
+    cnt = 0
+    for replicate_group in sample_name_2_replicate_groups[
+        "replicate_group_name"
+    ].unique():
+        list_of_compound_intensities = pd.DataFrame(
+            columns=["Metabolite", "Formula", "Intensities"]
+        )
+        for sample_name_index, sample_name in sample_name_2_replicate_groups[
+            sample_name_2_replicate_groups["replicate_group_name"]
+            == replicate_group
+        ].iterrows():
+            for met_index, met in extracted_data_all[
+                extracted_data_all["sample_group_name"]
+                == sample_name["sample_group_name"]
+            ].iterrows():
+                list_of_compound_intensities = (
+                    list_of_compound_intensities.append(
+                        pd.DataFrame(
+                            [
+                                {
+                                    "Metabolite": met["Metabolite"],
+                                    "Formula": met["Formula"],
+                                    "Intensities": met["Intensity"],
+                                }
+                            ]
+                        ),
+                        ignore_index=True,
+                    )
+                )
+        for met_name in list_of_compound_intensities["Metabolite"].unique():
+            intensities = list_of_compound_intensities[
+                list_of_compound_intensities["Metabolite"] == met_name
+            ]
+            if len(intensities) >= min_reps:
+                mean = np.mean(intensities["Intensities"])
+                var = np.var(intensities["Intensities"])
+                rsd = np.sqrt(var) / mean
+                stats_all_dict[cnt] = {
+                    "replicate_group_name": replicate_group,
+                    "Metabolite": met_name,
+                    "Formula": list(
+                        list_of_compound_intensities[
+                            list_of_compound_intensities["Metabolite"]
+                            == met_name
+                        ]["Formula"]
+                    )[0],
+                    "Mean": mean,
+                    "Variance": var,
+                    "RSD": rsd,
+                }
+                cnt = cnt + 1
+    return pd.DataFrame.from_dict(stats_all_dict, "index")
