065a65b630bb8a5950650a88d9c40a0d1d5c3f27
mmattano <57752712+mmattano@users.noreply.github.com>
2021-01-21 19:24:51 +0100

    fix: removed normalization from this folder

--- BFAIR/FIA_MS/__init__.py
+++ BFAIR/FIA_MS/__init__.py
@@ -2,24 +2,10 @@ from BFAIR.FIA_MS.import_statistics import (
     extractNamesAndIntensities,
     calculateMeanVarRSD,
 )
-from BFAIR.FIA_MS.normalization import (
-    min_max_norm,
-    tmi_norm,
-    biomass_tmi_norm,
-    biomass_formula_tmi_norm,
-    amino_acid_tmi_norm,
-    pqn_norm,
-)
 
 __version__ = "0.0.2"
 
 __all__ = [
     "extractNamesAndIntensities",
     "calculateMeanVarRSD",
-    "min_max_norm",
-    "tmi_norm",
-    "biomass_tmi_norm",
-    "biomass_formula_tmi_norm",
-    "amino_acid_tmi_norm",
-    "pqn_norm",
 ]
