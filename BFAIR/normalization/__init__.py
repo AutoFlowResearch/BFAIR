ae330e056f9765b67a9606471d47aeb098be20a1
mmattano <57752712+mmattano@users.noreply.github.com>
2021-01-22 19:43:47 +0100

    fix: and more typos

--- BFAIR/normalization/__init__.py
+++ BFAIR/normalization/__init__.py
@@ -1,5 +1,6 @@
-from BFAIR.normalization.normalization import (
-    min_max_normtmi_norm,
+from BFAIR.normalization import (
+    min_max_norm,
+    tmi_norm,
     biomass_tmi_norm,
     biomass_formula_tmi_norm,
     amino_acid_tmi_norm,
