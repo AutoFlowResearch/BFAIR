17b66f5c2b59d9fd69f6144501f6dd5d85fc9b48
Matthias Mattanovich <57752712+mmattano@users.noreply.github.com>
2021-01-28 11:10:05 +0100

    AUT-665 (#26)
    * feat: FIA_MS_tools
    
    * tests: added some initial tests
    
    * tests: added test data
    
    * Update test_FIA_MS_tools.py
    
    * fix: adjusted requirements and added setup file
    
    * fix: adjusted tests
    
    * Update test_FIA_MS_tools.py
    
    * tests: fixed errors in csv import and column selection
    
    * tests: it takes too long for now; fint a solution!
    
    * Update test_FIA_MS_tools.py
    
    * Update test_FIA_MS_tools.py
    
    * Update test_FIA_MS_tools.py
    
    * fix: generate.py file was not flake8 conform
    
    * fix: AUT-726: rename folder to BFAIR
    
    * tests: removed excess testing data
    
    Got it as a backup on my computer, would be cool to have more tests in there. Maybe not use HumanSerum since that one takes the longes
    
    * tests: changed to accommodate smaller dataset
    
    * tests: forgot to uncomment some variable assignments
    
    * adjustment for CircleCI
    
    * fix: flake8 issues
    
    * feat: added example notebook (with example data)
    
    * Update __init__.py
    
    * Update __init__.py
    
    * fix: repositioned version number
    
    * feat: added normalization methods
    
    * fix: re-named FIA_MS_tools to import_statistics
    
    * fix: changed method import
    
    * fix: changes in reference
    
    * fix: removed normalization from this folder
    
    * fix: removed notmalization module
    
    * fix: typo in the example notebook
    
    * fix: added docstrings
    
    * tests: added direct comparisons of outputs
    
    * tests: changed to "AlmostEqual" because it didn't pass CircleCI
    
    * tests: issues with comparing the dfs
    
    * tests: use different assertion function
    
    * Update test_FIA_MS_tools.py
    
    * Update test_FIA_MS_tools.py
    
    * tests: added new test function (I think)
    
    * tests: removed unused data
    
    * Adjusted docstrings and typos
    
    * fix: changed some typos
    
    * Update import_statistics.py
    
    * Update import_statistics.py
    
    * fix: setup issues

---
+++ BFAIR/FIA_MS/__init__.py
@@ -0,0 +1,11 @@
+from BFAIR.FIA_MS.import_statistics import (
+    extractNamesAndIntensities,
+    calculateMeanVarRSD,
+)
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "extractNamesAndIntensities",
+    "calculateMeanVarRSD",
+]
