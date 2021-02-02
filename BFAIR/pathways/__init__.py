7b68c8f14643a4c749d919357ab42f825c7a4837
ri-heme <72443078+ri-heme@users.noreply.github.com>
2021-02-02 22:11:06 +0100

    feat: visualization module
    Visualization module to plot the results of rule simulations.

--- BFAIR/pathways/__init__.py
+++ BFAIR/pathways/__init__.py
@@ -3,6 +3,7 @@
 The pathways module offers tools to assess off-target enzymatic activity in heterologous pathways.
 """
 
-from BFAIR.pathways.rules import RuleLibrary
+__all__ = ["RuleLibrary", "plot_pathway"]
 
-__all__ = ["RuleLibrary"]
+from BFAIR.pathways.rules import RuleLibrary
+from BFAIR.pathways.viz import plot_pathway
