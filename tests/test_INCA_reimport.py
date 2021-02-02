e52aae3fefc711359d0d6232d1eb102b9314b588
mmattano <57752712+mmattano@users.noreply.github.com>
2021-02-02 18:33:48 +0100

    tests: implemented unit tests and added data

---
+++ tests/test_INCA_reimport.py
@@ -0,0 +1,228 @@
+import unittest
+import pickle
+import pathlib
+from BFAIR.INCA import INCA_reimport
+
+current_dir = str(pathlib.Path(__file__).parent.absolute())
+
+
+class test_methods(unittest.TestCase):
+    def setUp(self):
+        file_obj = open(
+            current_dir
+            + "/test_data/MFA_modelInputsData/reimport_test_data.obj",
+            "rb",
+        )
+        (
+            filename,
+            simulation_info,
+            simulation_id,
+            info,
+            parallel,
+            non_stationary,
+            m,
+            f,
+            model_info,
+            simulationParameters,
+            f_mnt_info,
+            f_mnt_res_info,
+            f_par_info,
+            fittedData,
+            fittedFluxes,
+            fittedFragments,
+            fittedMeasuredFluxes,
+            fittedMeasuredFragments,
+            fittedMeasuredFluxResiduals,
+            fittedMeasuredFragmentResiduals,
+            simulationParameters,
+        ) = pickle.load(file_obj)
+        file_obj.close()
+
+        self.filename = filename
+        self.simulation_info = simulation_info
+        self.simulation_id = simulation_id
+        self.info = info
+        self.parallel = parallel
+        self.non_stationary = non_stationary
+        self.m = m
+        self.f = f
+        self.model_info = model_info
+        self.f_mnt_info = f_mnt_info
+        self.f_mnt_res_info = f_mnt_res_info
+        self.f_par_info = f_par_info
+        self.fittedData = fittedData
+        self.fittedFluxes = fittedFluxes
+        self.fittedFragments = fittedFragments
+        self.fittedMeasuredFluxes = fittedMeasuredFluxes
+        self.fittedMeasuredFragments = fittedMeasuredFragments
+        self.fittedMeasuredFluxResiduals = fittedMeasuredFluxResiduals
+        self.fittedMeasuredFragmentResiduals = fittedMeasuredFragmentResiduals
+        self.simulationParameters = simulationParameters
+        self.INCA_reimport = INCA_reimport()
+
+    """
+    The general instance tests will be added/activated once the problem in
+    the other INCA module is solved.
+    """
+
+    def test_extract_file_info(self):
+        info = self.INCA_reimport.extract_file_info(self.filename)
+        info_ = self.info
+        self.assertEqual(info, info_)
+
+    def test_det_simulation_type(self):
+        parallel, non_stationary = self.INCA_reimport.det_simulation_type(
+            self.simulation_info
+        )
+        parallel_ = self.parallel
+        non_stationary_ = self.non_stationary
+        self.assertEqual(parallel, parallel_)
+        self.assertEqual(non_stationary, non_stationary_)
+
+    def test_data_extraction(self):
+        m, f = self.INCA_reimport.data_extraction(self.filename)
+        m_ = self.m
+        f_ = self.f
+        self.assertEqual(m, m_)
+        self.assertEqual(f, f_)
+
+    def test_extract_model_info(self):
+        model_info = self.INCA_reimport.extract_model_info(self.m)
+        model_info_ = self.model_info
+        self.assertEqual(model_info, model_info_)
+
+    def test_extract_sim_params(self):
+        simulationParameters = self.INCA_reimport.extract_sim_params(
+            self.simulation_id, self.info, self.m, self.filename
+        )
+        simulationParameters_ = self.simulationParameters
+        self.assertEqual(simulationParameters, simulationParameters_)
+
+    def test_simulationParameters_testdata(self):
+        simulationParameters_ = self.simulationParameters
+        print(len(simulationParameters_))
+
+    def test_extract_base_stats(self):
+        fittedData = self.INCA_reimport.extract_base_stats(
+            self.f, self.simulation_id, self.info
+        )
+        fittedData_ = self.fittedData
+        self.assertEqual(fittedData, fittedData_)
+
+    def test_fittedData_testdata(self):
+        fittedData_ = self.fittedData
+        print(len(fittedData_))
+
+    def test_get_fit_info(self):
+        f_mnt_info = self.INCA_reimport.get_fit_info(self.f)
+        f_mnt_info_ = self.f_mnt_info
+        self.assertEqual(f_mnt_info, f_mnt_info_)
+
+    def test_sort_fit_info(self):
+        (
+            fittedMeasuredFluxes,
+            fittedMeasuredFragments,
+        ) = self.INCA_reimport.sort_fit_info(
+            self.f_mnt_info, self.simulation_info, self.fittedData
+        )
+        fittedMeasuredFluxes_ = self.fittedMeasuredFluxes
+        fittedMeasuredFragments_ = self.fittedMeasuredFragments
+        self.assertEqual(fittedMeasuredFluxes, fittedMeasuredFluxes_)
+        self.assertEqual(fittedMeasuredFragments, fittedMeasuredFragments_)
+
+    def test_fittedMeasuredFluxes_testdata(self):
+        fittedMeasuredFluxes_ = self.fittedMeasuredFluxes
+        print(len(fittedMeasuredFluxes_))
+
+    def test_fittedMeasuredFragments_testdata(self):
+        fittedMeasuredFragments_ = self.fittedMeasuredFragments
+        print(len(fittedMeasuredFragments_))
+
+    def test_get_residuals_info(self):
+        f_mnt_res_info = self.INCA_reimport.get_residuals_info(
+            self.f, self.simulation_info
+        )
+        f_mnt_res_info_ = self.f_mnt_res_info
+        self.assertEqual(f_mnt_res_info, f_mnt_res_info_)
+
+    def test_sort_residual_info(self):
+        (
+            fittedMeasuredFluxResiduals,
+            fittedMeasuredFragmentResiduals,
+        ) = self.INCA_reimport.sort_residual_info(
+            self.f_mnt_res_info, self.simulation_info, self.fittedData
+        )
+        fittedMeasuredFluxResiduals_ = self.fittedMeasuredFluxResiduals
+        fittedMeasuredFragmentResiduals_ = self.fittedMeasuredFragmentResiduals
+        self.assertEqual(
+            fittedMeasuredFluxResiduals, fittedMeasuredFluxResiduals_
+        )
+        self.assertEqual(
+            fittedMeasuredFragmentResiduals, fittedMeasuredFragmentResiduals_
+        )
+
+    def test_fittedMeasuredFluxResiduals_testdata(self):
+        fittedMeasuredFluxResiduals_ = self.fittedMeasuredFluxResiduals
+        print(len(fittedMeasuredFluxResiduals_))
+
+    def test_fittedMeasuredFragmentResiduals_testdata(self):
+        fittedMeasuredFragmentResiduals_ = self.fittedMeasuredFragmentResiduals
+        print(len(fittedMeasuredFragmentResiduals_))
+
+    def test_get_fitted_parameters(self):
+        f_par_info = self.INCA_reimport.get_fitted_parameters(
+            self.f, self.simulation_info
+        )
+        f_par_info_ = self.f_par_info
+        self.assertEqual(f_par_info, f_par_info_)
+
+    def test_sort_parameter_info(self):
+        fittedFluxes, fittedFragments = self.INCA_reimport.sort_parameter_info(
+            self.f_par_info, self.simulation_info, self.fittedData
+        )
+        fittedFluxes_ = self.fittedFluxes
+        fittedFragments_ = self.fittedFragments
+        self.assertEqual(fittedFluxes, fittedFluxes_)
+        self.assertEqual(fittedFragments, fittedFragments_)
+
+    def test_fittedFluxes_testdata(self):
+        fittedFluxes_ = self.fittedFluxes
+        print(len(fittedFluxes_))
+
+    def test_fittedFragments_testdata(self):
+        fittedFragments_ = self.fittedFragments
+        print(len(fittedFragments_))
+
+    def test_reimport(self):
+        (
+            fittedData,
+            fittedFluxes,
+            fittedFragments,
+            fittedMeasuredFluxes,
+            fittedMeasuredFragments,
+            fittedMeasuredFluxResiduals,
+            fittedMeasuredFragmentResiduals,
+            simulationParameters,
+        ) = self.INCA_reimport.reimport(
+            self.filename, self.simulation_info, self.simulation_id
+        )
+        fittedData_ = self.fittedData
+        fittedFluxes_ = self.fittedFluxes
+        fittedFragments_ = self.fittedFragments
+        fittedMeasuredFluxes_ = self.fittedMeasuredFluxes
+        fittedMeasuredFragments_ = self.fittedMeasuredFragments
+        fittedMeasuredFluxResiduals_ = self.fittedMeasuredFluxResiduals
+        fittedMeasuredFragmentResiduals_ = self.fittedMeasuredFragmentResiduals
+        simulationParameters_ = self.simulationParameters
+        self.assertEqual(fittedData, fittedData_)
+        self.assertEqual(fittedFluxes, fittedFluxes_)
+        self.assertEqual(fittedFragments, fittedFragments_)
+        self.assertEqual(fittedMeasuredFluxes, fittedMeasuredFluxes_)
+        self.assertEqual(fittedMeasuredFragments, fittedMeasuredFragments_)
+        self.assertEqual(
+            fittedMeasuredFluxResiduals, fittedMeasuredFluxResiduals_
+        )
+        self.assertEqual(
+            fittedMeasuredFragmentResiduals, fittedMeasuredFragmentResiduals_
+        )
+        self.assertEqual(simulationParameters, simulationParameters_)
---
+++ tests/test_data/MFA_modelInputsData/generate_reimport_test_data.py
@@ -0,0 +1,76 @@
+# generate test_data
+# Last date : 02.02.2020
+# By : Matthias Mattanovich (matmat@biosustain.dtu.dk)
+# This script is intended to generate sample data and save them into the
+# test_data file. The saved objects will then be used to test the
+# INCA_reimport using unit testing.
+import pickle
+import pandas as pd
+from BFAIR.INCA import INCA_reimport
+
+
+pd.set_option("mode.chained_assignment", None)
+# Use pickle to save python variables
+filehandler = open("reimport_test_data.obj", "wb")
+
+INCA_reimport = INCA_reimport()
+
+# Load the data
+filename = 'TestFile.mat'
+simulation_info = pd.read_csv(
+    'data-1604345289079.csv'
+    )
+simulation_id = 'WTEColi_113C80_U13C20_01'
+
+# Generate variables to save
+# Succession of functions
+info = INCA_reimport.extract_file_info(filename)
+parallel, non_stationary = INCA_reimport.det_simulation_type(simulation_info)
+m, f = INCA_reimport.data_extraction(filename)
+model_info = INCA_reimport.extract_model_info(m)
+simulationParameters = INCA_reimport.extract_sim_params(
+    simulation_id, info, m, filename
+    )
+fittedData = INCA_reimport.extract_base_stats(f, simulation_id, info)
+f_mnt_info = INCA_reimport.get_fit_info(f)
+fittedMeasuredFluxes, fittedMeasuredFragments = INCA_reimport.sort_fit_info(
+    f_mnt_info, simulation_info, fittedData
+    )
+f_mnt_res_info = INCA_reimport.get_residuals_info(f, simulation_info)
+fittedMeasuredFluxResiduals, fittedMeasuredFragmentResiduals = \
+    INCA_reimport.sort_residual_info(
+        f_mnt_res_info, simulation_info, fittedData
+        )
+f_par_info = INCA_reimport.get_fitted_parameters(f, simulation_info)
+fittedFluxes, fittedFragments = INCA_reimport.sort_parameter_info(
+    f_par_info, simulation_info, fittedData
+    )
+
+
+pickle.dump(
+    [
+            filename,
+            simulation_info,
+            simulation_id,
+            info,
+            parallel,
+            non_stationary,
+            m,
+            f,
+            model_info,
+            f_mnt_info,
+            f_mnt_res_info,
+            f_par_info,
+            fittedData,
+            fittedFluxes,
+            fittedFragments,
+            fittedMeasuredFluxes,
+            fittedMeasuredFragments,
+            fittedMeasuredFluxResiduals,
+            fittedMeasuredFragmentResiduals,
+            simulationParameters,
+    ],
+    filehandler,
+)
+
+filehandler.close()
