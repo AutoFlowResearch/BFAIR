2954f6333b640976814f0bb0a396335ebdef9562
mmattano <57752712+mmattano@users.noreply.github.com>
2021-02-12 12:59:55 +0100

    tests: re-generated test data

--- tests/test_data/MFA_modelInputsData/generate_reimport_test_data.py
+++ tests/test_data/MFA_modelInputsData/generate_reimport_test_data.py
@@ -1,5 +1,5 @@
 # generate test_data
-# Last date : 09.02.2021
+# Last date : 12.02.2021
 # By : Matthias Mattanovich (matmat@biosustain.dtu.dk)
 # This script is intended to generate sample data and save them into the
 # test_data file. The saved objects will then be used to test the
