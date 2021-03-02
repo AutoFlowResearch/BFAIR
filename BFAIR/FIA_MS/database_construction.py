42a7aa9d52458ee063a30b6f6a258fbece7829e6
mmattano <57752712+mmattano@users.noreply.github.com>
2021-03-02 14:24:58 +0100

    feat: improved construction file

--- BFAIR/FIA_MS/database_construction.py
+++ BFAIR/FIA_MS/database_construction.py
@@ -9,10 +9,34 @@ INVALID_FORMULA_STR = ["(", "Generic", "R", "X"]
 
 
 def print_formula(elements):
-    return "".join([f"{k}{int(v)}" for k, v in elements.items()])
+    """
+    Parameters
+    ----------
+    elements: dict
+        The elements that form the metabolite and their corresponding amount
+
+    Returns
+    -------
+    formula: str
+        The formula of the metabolite
+    """
+    formula = "".join([f"{k}{int(v)}" for k, v in elements.items()])
+    return formula
 
 
 def zero_charge(metabolite):
+    """
+    Parameters
+    ----------
+    metabolite: cobra.Metabolite
+        A metabolite in the cobra format including additional information
+        (formula, charge, elements)
+
+    Returns
+    -------
+    formula: str
+        The formula of the metabolite
+    """
     formula = Formula(metabolite.formula)
     if metabolite.charge != 0:
         if "desulfurated" in metabolite.name:
@@ -26,45 +50,111 @@ def zero_charge(metabolite):
 
 
 def is_valid(metabolite):
+    """
+    Parameters
+    ----------
+    metabolite: cobra.Metabolite
+        A metabolite in the cobra format including additional information
+        (formula, charge, elements)
+
+    Returns
+    -------
+    boolean
+        Valid or invalid metabolite
+    """
     if not metabolite.formula:
         return False
     for string in INVALID_FORMULA_STR:
         if string in metabolite.formula:
             return False
+    return True
 
 
 def make_struct(formulas, ids):
+    """
+    Parameters
+    ----------
+    formulas: list
+        List of the formulas of the metabolites in the model
+    ids: list
+        List if the ids of the metabolites in the model
+
+    Returns
+    -------
+    df_formulas: pandas.DataFrame
+        Dataframe with information about the metabolites in the model.
+        The masses are all set to 0
+    """
     masses = [0] * len(formulas)
-    df_formulas = pd.DataFrame(columns=["mass", "formula", "id"])
-    df_formulas["mass"] = masses
-    df_formulas["formula"] = formulas
-    df_formulas["id"] = ids
+    df_formulas = pd.DataFrame(
+        {"mass": masses, "formula": formulas, "id": ids}
+    )
     return df_formulas
 
 
 def make_mapping(df_formulas):
+    """
+    Parameters
+    ----------
+    df_formulas: pandas.DataFrame
+        Dataframe with information about the metabolites in the model.
+        The masses are all set to 0
+
+    Returns
+    -------
+    df_mapping: pandas.DataFrame
+        Dataframe with information about overlapping metabolite formulas in
+        the model. These include the same metabolites in different
+        compartments and metabolites with the same composition but different
+        conformations
+    """
     map_id = {}
     for formula, df in df_formulas.groupby("formula"):
         map_id[formula] = "\t".join(df["id"])
     df_mapping = df_formulas.drop_duplicates(subset=["formula"])
-    df_mapping["id"] = df_mapping["formula"].map(map_id)
+    df_mapping.loc[:, "id"] = df_mapping["formula"].map(map_id)
     return df_mapping
 
 
 def store_struct(df_formulas, name, dirpath):
+    """
+    Parameters
+    ----------
+    df_formulas: pandas.DataFrame
+        Dataframe with information about the metabolites in the model.
+        The masses are all set to 0
+    name: str
+        Name of the database
+    dirpath: path
+        Path to where the database should be saved
+    """
     df_struct_mapping = pd.DataFrame(
-        columns=["id", "formula", "smiles", "inchi"]
+        {
+            "id": df_formulas["id"],
+            "formula": df_formulas["formula"],
+            "smiles": ["smiles"] * len(df_formulas["id"]),
+            "inchi": ["inchi"] * len(df_formulas["id"]),
+        }
     )
-    df_struct_mapping["formula"] = df_formulas["formula"]
-    df_struct_mapping["id"] = df_formulas["id"]
-    df_struct_mapping["smiles"] = "smiles"
-    df_struct_mapping["inchi"] = "inchi"
     df_struct_mapping.to_csv(
         f"{dirpath}/{name}_struct.tsv", sep="\t", header=None, index=None
     )
 
 
 def store_mapping(df_mapping, name, dirpath):
+    """
+    Parameters
+    ----------
+    df_mapping: pandas.DataFrame
+        Dataframe with information about overlapping metabolite formulas
+        in the model. These include the same metabolites in different
+        compartments and metabolites with the same composition but
+        different conformations
+    name: str
+        Name of the database
+    dirpath: path
+        Path to where the database should be saved
+    """
     filename_mapping_csv = f"{dirpath}/{name}_mapping_csv.tsv"
     filename_mapping = f"{dirpath}/{name}_mapping.tsv"
     df_mapping.to_csv(
@@ -73,11 +163,21 @@ def store_mapping(df_mapping, name, dirpath):
     with open(filename_mapping_csv) as f:
         with open(filename_mapping, "w") as fm:
             fm.write(f"database_name	{name}\ndatabase_version	{name}\n")
-            for line in f:
-                fm.write(line.replace('"', ""))
+            for l in f:
+                fm.write(l.replace('"', ""))
 
 
 def create_database(metabolites, name, dirpath):
+    """
+    Parameters
+    ----------
+    metabolites: cobra.DictList or list
+        List of the metabolites in the model
+    name: str
+        Name of the database
+    dirpath: path
+        Path to where the database should be saved
+    """
     formulas = []
     ids = []
     for m in metabolites:
