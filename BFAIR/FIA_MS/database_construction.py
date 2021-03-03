b49015e87a39bbff30979f48b0d0535632b761df
mmattano <57752712+mmattano@users.noreply.github.com>
2021-03-03 13:53:04 +0100

    feat: added documentation

--- BFAIR/FIA_MS/database_construction.py
+++ BFAIR/FIA_MS/database_construction.py
@@ -10,6 +10,9 @@ INVALID_FORMULA_STR = ["(", "Generic", "R", "X"]
 
 def print_formula(elements):
     """
+    The input dictionary, atoms and their amount, is processed to produce
+    the chemical formula as a string
+
     Parameters
     ----------
     elements: dict
@@ -26,6 +29,9 @@ def print_formula(elements):
 
 def zero_charge(metabolite):
     """
+    The chemical formulas of the input metabolites are modified to not carry
+    any charge
+
     Parameters
     ----------
     metabolite: cobra.Metabolite
@@ -51,6 +57,10 @@ def zero_charge(metabolite):
 
 def is_valid(metabolite):
     """
+    The validity of the input metabolites is checked. It's invalid if it
+    does not have any formula annotated of if the formula includes previously
+    defined invalid symbols
+
     Parameters
     ----------
     metabolite: cobra.Metabolite
@@ -72,6 +82,9 @@ def is_valid(metabolite):
 
 def make_struct(formulas, ids):
     """
+    A dataframe combining the chemical formula and the metabolite's ids
+    is constructed
+
     Parameters
     ----------
     formulas: list
@@ -94,6 +107,8 @@ def make_struct(formulas, ids):
 
 def make_mapping(df_formulas):
     """
+    The chemical formulas are checked for overlaps
+
     Parameters
     ----------
     df_formulas: pandas.DataFrame
@@ -112,12 +127,14 @@ def make_mapping(df_formulas):
     for formula, df in df_formulas.groupby("formula"):
         map_id[formula] = "\t".join(df["id"])
     df_mapping = df_formulas.drop_duplicates(subset=["formula"])
-    df_mapping.loc[:, "id"] = df_mapping["formula"].map(map_id)
+    df_mapping = df_mapping.assign(id=df_mapping["formula"].map(map_id))
     return df_mapping
 
 
 def store_struct(df_formulas, name, dirpath):
     """
+    The metabolite's ids and formulas are saved in a .tsv file
+
     Parameters
     ----------
     df_formulas: pandas.DataFrame
@@ -143,6 +160,9 @@ def store_struct(df_formulas, name, dirpath):
 
 def store_mapping(df_mapping, name, dirpath):
     """
+    The information about which metabolite's chemical formulas overlap is
+    saved in a .tsv file
+
     Parameters
     ----------
     df_mapping: pandas.DataFrame
@@ -163,12 +183,43 @@ def store_mapping(df_mapping, name, dirpath):
     with open(filename_mapping_csv) as f:
         with open(filename_mapping, "w") as fm:
             fm.write(f"database_name	{name}\ndatabase_version	{name}\n")
-            for l in f:
-                fm.write(l.replace('"', ""))
+            for line in f:
+                fm.write(line.replace('"', ""))
+
+
+def import_mapping_tsv(file):
+    """
+    Helper function to re-import the mapping.tsv files into a
+    dataframe for testing
+
+    Parameters
+    ----------
+        file: str
+            filename if needed + path
+
+    Returns
+    -------
+        df: pandas.DataFrame
+            a dataframe containing the mapping.tsv file and nan in
+            unused positions
+    """
+    # Loop the data lines
+    with open(file, 'r') as temp_f:
+        # get No of columns in each line
+        col_count = [len(line.split("\t")) for line in temp_f.readlines()]
+    # Generate column names  (names will be 0, 1, 2, ..., maximum columns - 1)
+    column_names = [i for i in range(0, max(col_count))]
+    # Read csv
+    df = pd.read_csv(file, sep="\t", header=None, names=column_names)
+    return df
 
 
 def create_database(metabolites, name, dirpath):
     """
+    Summary function that creates the .tsv database files from lists
+    of metabolites with chemical formulas saved in the cobra format
+    (or equivalent)
+
     Parameters
     ----------
     metabolites: cobra.DictList or list
