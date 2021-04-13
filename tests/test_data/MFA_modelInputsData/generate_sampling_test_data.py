623111f61da5f91eb298d654dfb5b8b21febff06
mmattano <57752712+mmattano@users.noreply.github.com>
2021-04-13 17:51:41 +0200

    Update generate_sampling_test_data.py

--- tests/test_data/MFA_modelInputsData/generate_sampling_test_data.py
+++ tests/test_data/MFA_modelInputsData/generate_sampling_test_data.py
@@ -7,14 +7,38 @@
 import pickle
 import pandas as pd
 from BFAIR.INCA.sampling import (
-
+    add_constraints,
+    find_biomass_reaction,
+    get_min_solution_val,
+    replace_biomass_rxn_name,
+    add_feasible_constraints,
+    reshape_fluxes_escher,
+    bound_relaxation,
 )
 
 pd.set_option("mode.chained_assignment", None)
 # Use pickle to save python variables
 filehandler = open("sampling_test_data.obj", "wb")
 
-
+add_constraints(model_input, fittedFluxes)
+find_biomass_reaction(
+    model,
+    biomass_string=['Biomass', 'BIOMASS', 'biomass']
+)
+get_min_solution_val(fittedFluxes, biomass_string='Biomass')
+replace_biomass_rxn_name(
+    fittedFluxes,
+    biomass_rxn_name,
+    biomass_string='Biomass',
+)
+add_feasible_constraints(model_input, fittedFluxes, min_val=0)
+reshape_fluxes_escher(sampled_fluxes)
+bound_relaxation(
+    infeasible_model,
+    fittedFluxes,
+    destructive=True,
+    fluxes_to_ignore=[],
+)
 
 pickle.dump(
     [
