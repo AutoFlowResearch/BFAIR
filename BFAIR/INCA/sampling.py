5b2d2172762408bcc94ad51a29a314dd003ac9da
mmattano <57752712+mmattano@users.noreply.github.com>
2021-04-13 12:24:24 +0200

    fix: got relaxation to work

--- BFAIR/INCA/sampling.py
+++ BFAIR/INCA/sampling.py
@@ -147,7 +147,7 @@ def _extract_bound_violations(grb_model, variables, negative_slack_prefix, posit
     return violations
 
 @timer
-def bound_relaxation(infeasible_model, fittedFluxes, destructive=True):
+def bound_relaxation(infeasible_model, fittedFluxes, destructive=True, fluxes_to_ignore=[]):
     model = infeasible_model
     model.solver = 'optlang-gurobi'
     if model.solver.interface.__name__ != "optlang.gurobi_interface":
@@ -157,6 +157,11 @@ def bound_relaxation(infeasible_model, fittedFluxes, destructive=True):
     grb_model: GRBModel = model.solver.problem.copy()
     MFA_fluxes = _reshape_fluxes(fittedFluxes)
     reactions_to_relax = list(MFA_fluxes.keys())
+
+    # remove fluxes to ignore
+    if fluxes_to_ignore:
+        for flux_to_ignore in fluxes_to_ignore:
+            reactions_to_relax.remove(flux_to_ignore)
 
     # There are no metabolites to relax to we leave relax_cons empty
     relax_cons = []
@@ -189,31 +194,42 @@ def bound_relaxation(infeasible_model, fittedFluxes, destructive=True):
     # check if relaxation was successful
     if relax_obj <= 0:
         raise Infeasible("Failed to create the feasibility relaxation!")
-
     grb_model.optimize()
 
     # transfer/record the lower/upper bound changes to the cobra model
     if relax_vars:
         variables = [var.VarName for var in relax_vars]
         rows = []
-        violations = _extract_bound_violations(grb_model, variables, "ArtL_", "ArtU_")
+        violations = _extract_bound_violations(
+            grb_model, variables, "ArtL_", "ArtU_"
+        )
         for rxn_id, lb_change, ub_change in violations:
+            try:
+                subsystem = model.reactions.get_by_id(rxn_id).subsystem
+            except KeyError:
+                subsystem = model.reactions.get_by_id(
+                    re.match(
+                        '.+?(?=_reverse_)', rxn_id
+                    )[0]).subsystem
+            rows.append(
+                {
+                    "reaction": rxn_id,
+                    "lb_change": lb_change,
+                    "ub_change": ub_change,
+                    "subsystem": subsystem,
+                }
+            )
             if destructive:
                 if '_reverse_' in rxn_id:
+                    # reverse reactions are only temporary for optimization,
+                    # so we have to adjust the corresponding original reaction
                     rxn_id = re.match('.+?(?=_reverse_)', rxn_id)[0]
-                    lb_change = -ub_change
-                    ub_change = 0
+                    lb_change, ub_change = -ub_change, -lb_change
                 lb = model.reactions.get_by_id(rxn_id).lower_bound
                 ub = model.reactions.get_by_id(rxn_id).upper_bound
                 model.reactions.get_by_id(rxn_id).lower_bound = lb + lb_change
                 model.reactions.get_by_id(rxn_id).upper_bound = ub + ub_change
-            rows.append(
-                {
-                    "reaction": rxn_id,
-                    "bound_change": lb_change + ub_change,
-                    "subsystem": model.reactions.get_by_id(rxn_id).subsystem,
-                }
-            )
+
         cons_table = pd.DataFrame.from_records(rows, index="reaction")
     else:
         cons_table = None
