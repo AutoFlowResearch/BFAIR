0d3939cb4ff110105b1946bd76d2ebd10adfdb9b
mmattano <57752712+mmattano@users.noreply.github.com>
2021-04-13 18:44:20 +0200

    Update generate_sampling_test_data.py

--- tests/test_data/MFA_modelInputsData/generate_sampling_test_data.py
+++ tests/test_data/MFA_modelInputsData/generate_sampling_test_data.py
@@ -6,6 +6,8 @@
 # MFA_sampling functions using unit testing.
 import pickle
 import pandas as pd
+import cobra
+from BFAIR.INCA import INCA_reimport
 from BFAIR.INCA.sampling import (
     add_constraints,
     find_biomass_reaction,
@@ -20,20 +22,48 @@ pd.set_option("mode.chained_assignment", None)
 # Use pickle to save python variables
 filehandler = open("sampling_test_data.obj", "wb")
 
-add_constraints(model_input, fittedFluxes)
+filename = 'data/MFA_modelInputsData/TestFile.mat'
+simulation_info = pd.read_csv(
+    'data/MFA_modelInputsData/Re-import/experimentalMS_data_I.csv'
+    )
+simulation_id = 'WTEColi_113C80_U13C20_01'
+model = cobra.io.load_json_model(
+    'data/FIA_MS_example/database_files/iJO1366.json'
+    )
+reimport_data = INCA_reimport()
+(fittedData,
+ fittedFluxes,
+ fittedFragments,
+ fittedMeasuredFluxes,
+ fittedMeasuredFragments,
+ fittedMeasuredFluxResiduals,
+ fittedMeasuredFragmentResiduals,
+ simulationParameters) = reimport_data.reimport(
+    filename,
+    simulation_info,
+    simulation_id
+)
+
+model_input = model
+constrained_model = add_constraints(model_input, fittedFluxes)
 find_biomass_reaction(
     model,
     biomass_string=['Biomass', 'BIOMASS', 'biomass']
 )
-get_min_solution_val(fittedFluxes, biomass_string='Biomass')
-replace_biomass_rxn_name(
+min_val = get_min_solution_val(fittedFluxes, biomass_string='Biomass')
+fittedFluxes = replace_biomass_rxn_name(
     fittedFluxes,
-    biomass_rxn_name,
+    biomass_rxn_name='BIOMASS_Ec_iJO1366_core_53p95M',
     biomass_string='Biomass',
 )
-add_feasible_constraints(model_input, fittedFluxes, min_val=0)
-reshape_fluxes_escher(sampled_fluxes)
-bound_relaxation(
+model_input = model
+feasible_constrained_model, problems = add_feasible_constraints(
+    model_input, fittedFluxes, min_val=0)
+### add one sampling solution and one solution type solution
+fluxes_sampling = reshape_fluxes_escher(sampled_fluxes)
+fluxes_solution = reshape_fluxes_escher(solution)
+infeasible_model = model
+cons_table = bound_relaxation(
     infeasible_model,
     fittedFluxes,
     destructive=True,
@@ -42,7 +72,13 @@ bound_relaxation(
 
 pickle.dump(
     [
-        name,
+        constrained_model,
+        min_val,
+        fittedFluxes,
+        feasible_constrained_model,
+        problems,
+        fluxes_sampling,
+        cons_table,
     ],
     filehandler,
 )
