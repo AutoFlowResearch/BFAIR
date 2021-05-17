8915c2c610aefe7eb2a2b3e767afa0d0079a44b5
mmattano <57752712+mmattano@users.noreply.github.com>
2021-05-17 16:17:52 +0200

    feat: added and improved plotting methods

--- BFAIR/INCA/plotting.py
+++ BFAIR/INCA/plotting.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
 import pandas as pd
+import scipy
 import scipy.stats as stats
+from sklearn.neighbors import KernelDensity
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -31,7 +33,10 @@ def sampled_fluxes_minrange(sampled_fluxes, min_val, max_val):
 
     def in_between(column):
         return not all(column.between(min_val, max_val, inclusive=False))
-    criteria = list(sampled_fluxes.apply(in_between, axis=0, result_type='reduce'))
+
+    criteria = list(
+        sampled_fluxes.apply(in_between, axis=0, result_type="reduce")
+    )
     return sampled_fluxes[sampled_fluxes.columns[criteria]]
 
 
@@ -46,7 +51,7 @@ def show_reactions(reactions):
         List of reactions, e.g. output of `get_subsytem_reactions()`.
     """
     for cnt, rxn in enumerate(reactions):
-        print(f'{cnt}: {rxn}')
+        print(f"{cnt}: {rxn}")
 
 
 def _sampled_reaction_fit(sampled_values, alpha=1e-3):
@@ -59,9 +64,6 @@ def _sampled_reaction_fit(sampled_values, alpha=1e-3):
     sampled_values : pandas.Series
         Sampled values for one reaction.
 
-    sampled_value = sampled_fluxes[reactions[reaction_id]]
-    if all(v == 0 for v in sampled_value):
-        print(f'All sample values are 0 for {reactions[reaction_id]}')
     Returns
     -------
     sampled_reaction : list
@@ -71,21 +73,26 @@ def _sampled_reaction_fit(sampled_values, alpha=1e-3):
         Normal distribution fit (y-Values) over the sampled
         data.
     """
+    sampled_reaction = sorted(sampled_values)
+    k2, p = scipy.stats.normaltest(sampled_reaction)
+    if p < alpha:
+        norm_dist = False
+        model = KernelDensity(bandwidth=2, kernel="gaussian")
+        sample = np.array(sampled_reaction)
+        sample = sample.reshape((len(sample), 1))
+        model.fit(sample)
+        probabilities = model.score_samples(sample)
+        fit = np.exp(probabilities)
     else:
-        sampled_reaction, fit = _sampled_reaction_fit(
-            sampled_value,
+        norm_dist = True
+        fit = stats.norm.pdf(
+            sampled_reaction,
+            np.mean(sampled_reaction),
+            np.std(sampled_reaction),
         )
-        fig = plt.plot(sampled_reaction, fit,'-o', color='darksalmon')
-        plt.hist(sampled_reaction, bins=bins, 
-                 rwidth=.95, color='darkcyan', density=True)
-        plt.ylabel('Distribution [%]')
-        plt.xlabel('Flux [mmol * gDCW$^{-1}$ * h$^{-1}$]')
-        plt.title(reactions[reaction_id], size = 20)
-        return fig        
-        
-        
-    sampled_value = sampled_fluxes[reactions[reaction_id]]
-    if all(v == 0 for v in sampled_value):
+    return sampled_reaction, fit, norm_dist
+
+
 def plot_sampled_reaction_fluxes(
     sampled_fluxes, reactions, reaction_id, bins=10, alpha=1e-3
 ):
@@ -110,6 +117,32 @@ def plot_sampled_reaction_fluxes(
     fig : matplotlib.Figure
         Figure of sampled value distribution for one reaction.
     """
+    sampled_values = sampled_fluxes[reactions[reaction_id]]
+    if all(v == 0 for v in sampled_values):
+        print(f"All sample values are 0 for {reactions[reaction_id]}")
+    else:
+        sampled_reaction, fit, norm_dist = _sampled_reaction_fit(
+            sampled_values, alpha
+        )
+        fig = plt.plot(
+            sampled_reaction,
+            fit,
+            "-o",
+            color=("darksalmon" if norm_dist else "darkcyan"),
+        )
+        plt.hist(
+            sampled_reaction,
+            bins=bins,
+            rwidth=0.95,
+            color=("darkcyan" if norm_dist else "darksalmon"),
+            density=True,
+        )
+        plt.ylabel("Distribution [%]")
+        plt.xlabel("Flux [mmol * gDCW$^{-1}$ * h$^{-1}$]")
+        plt.title(reactions[reaction_id], size=20)
+        return fig
+
+
 def _subplot_sampled_reaction_fluxes(
     sampled_fluxes, reactions, reaction_id, bins=10, alpha=1e-3
 ):
@@ -134,23 +167,39 @@ def _subplot_sampled_reaction_fluxes(
     fig : matplotlib.Figure
         Figure of sampled value distribution for one reaction.
     """
+    sampled_values = sampled_fluxes[reactions[reaction_id]]
+    if all(v == 0 for v in sampled_values):
         fig = plt.plot(figsize=(15, 20))
-        plt.axis('off')
-        plt.text(0, -0.02, f'All sampled\nvalues are 0\nfor {reactions[reaction_id]}', ha='center', fontsize=20)
-    else:
-        sampled_reaction, fit = _sampled_reaction_fit(
-            sampled_value,
+        plt.axis("off")
+        plt.text(
+            0,
+            -0.02,
+            f"All sampled\nvalues are 0\nfor {reactions[reaction_id]}",
+            ha="center",
+            fontsize=20,
         )
-        fig = plt.plot(sampled_reaction, fit,'-o', color='darksalmon')
-        plt.hist(sampled_reaction, bins=bins, 
-                 rwidth=.95, color='darkcyan', density=True)
-        plt.title(reactions[reaction_id], size = 20)
-        return fig        
-        
-        
+    else:
+        sampled_reaction, fit, norm_dist = _sampled_reaction_fit(
+            sampled_values, alpha
+        )
+        fig = plt.plot(
+            sampled_reaction,
+            fit,
+            "-o",
+            color=("darksalmon" if norm_dist else "darkcyan"),
+        )
+        plt.hist(
+            sampled_reaction,
+            bins=bins,
+            rwidth=0.95,
+            color=("darkcyan" if norm_dist else "darksalmon"),
+            density=True,
+        )
+        plt.title(reactions[reaction_id], size=20)
+        return fig
+
+
 def plot_all_subsystem_fluxes(sampled_fluxes, reactions, bins=10):
-    rows = math.ceil(len(reactions)/3)
-    fig, ax = plt.subplots(figsize=(15, 5 * (rows/2)))
     """
     Plots the distribution of the sampled values for all
     reactions in a selected subsytem as a histogram and a
@@ -171,15 +220,32 @@ def plot_all_subsystem_fluxes(sampled_fluxes, reactions, bins=10):
     fig : matplotlib.Figure
         Figure of sampled value distribution for one reaction.
     """
+    rows = math.ceil(len(reactions) / 3)
+    fig, ax = plt.subplots(figsize=(15, 5 * (rows / 2)))
     for i in range(0, len(reactions)):
-        plt.subplot(rows,3,i+1)
-        _subplot_sampled_reaction_fluxes(sampled_fluxes, reactions, reaction_id=i, bins=bins)
+        plt.subplot(rows, 3, i + 1)
+        _subplot_sampled_reaction_fluxes(
+            sampled_fluxes, reactions, reaction_id=i, bins=bins
+        )
     plt.tight_layout()
-    fig.text(0.5, -0.02, 'Flux [mmol * gDCW$^{-1}$ * h$^{-1}$]', ha='center', fontsize=20)
-    fig.text(-0.02, 0.5, 'Distribution [%]', va='center', rotation='vertical', fontsize=20)
-    return fig          
-        
-        
+    fig.text(
+        0.5,
+        -0.02,
+        "Flux [mmol * gDCW$^{-1}$ * h$^{-1}$]",
+        ha="center",
+        fontsize=20,
+    )
+    fig.text(
+        -0.02,
+        0.5,
+        "Distribution [%]",
+        va="center",
+        rotation="vertical",
+        fontsize=20,
+    )
+    return fig
+
+
 def show_subsystems(model):
     """
     Prints the names of all the subsystems in the model with
@@ -196,9 +262,9 @@ def show_subsystems(model):
         if subsystem not in subsystems:
             subsystems.append(subsystem)
     for cnt, sub in enumerate(subsystems):
-        print(f'{cnt}: {sub}')        
-        
-        
+        print(f"{cnt}: {sub}")
+
+
 def get_subsytem_reactions(model, subsystem_id):
     """
     Extracts the names of the reactions in a selected subsytem. Also
@@ -235,7 +301,6 @@ def get_subsytem_reactions(model, subsystem_id):
 
 
 def _reduce_sampled_fluxes(sampled_fluxes, reactions):
-    reactions_mask = [True if col in reactions else False for col in sampled_fluxes.columns]
     """
     Reduces the input dataframe of sampled fluxes to a subset of
     selected reactions.
@@ -253,9 +318,16 @@ def _reduce_sampled_fluxes(sampled_fluxes, reactions):
     re_arranged_df : pandas.Dataframe
         Sampled fluxes reduced to a specified set of reactions.
     """
+    reactions_mask = [
+        True if col in reactions else False for col in sampled_fluxes.columns
+    ]
     subsystem_df = sampled_fluxes[sampled_fluxes.columns[reactions_mask]]
-    re_arranged_df = subsystem_df.stack().reset_index(level=[1], name='Sampled Fluxes')
-    re_arranged_df = re_arranged_df.rename(columns = {'level_1':'Reaction'}).reset_index(drop=True)
+    re_arranged_df = subsystem_df.stack().reset_index(
+        level=[1], name="Sampled Fluxes"
+    )
+    re_arranged_df = re_arranged_df.rename(
+        columns={"level_1": "Reaction"}
+    ).reset_index(drop=True)
     return re_arranged_df
 
 
@@ -286,12 +358,18 @@ def plot_subsystem_fluxes(
     """
     fig = plt.plot(figsize=(15, 20))
     if no_zero_cols:
-        sampled_fluxes = sampled_fluxes.loc[:, (sampled_fluxes != 0).any(axis=0)]
-    reactions, subsystems = get_subsytem_reactions(model, sampled_fluxes, subsystem_id)
+        sampled_fluxes = sampled_fluxes.loc[
+            :, (sampled_fluxes != 0).any(axis=0)
+        ]
+    reactions, subsystems = get_subsytem_reactions(model, subsystem_id)
     re_arranged_df = _reduce_sampled_fluxes(sampled_fluxes, reactions)
-    sns.boxplot(x="Reaction", y="Sampled Fluxes", data=re_arranged_df, orient = 'v')
+    sns.boxplot(
+        x="Reaction", y="Sampled Fluxes", data=re_arranged_df, orient="v"
+    )
     plt.xticks(rotation=70)
-    plt.title(subsystems[subsystem_id], size = 20)
+    plt.title(subsystems[subsystem_id], size=20)
+    return fig
+
 
 # Flux split
 
@@ -325,7 +403,10 @@ def _prepare_input_fluxes(sampled_fluxes, flux):
         raise TypeError(
             f"The input is a '{type(flux)}', this type of object"
             " cannot be used here, please provide a pd.Series or"
-            "a column identifier (string)")
+            "a column identifier (string)"
+        )
+
+
 def calculate_split_ratio(
     sampled_fluxes,
     influx,
@@ -364,16 +445,19 @@ def calculate_split_ratio(
     split_flux1 = _prepare_input_fluxes(sampled_fluxes, split_flux1)
     mean_list = [np.abs(np.mean(split_flux1 / influx))]
     stdev_list = [np.std(split_flux1 / influx)]
-    index_list = [influx.name + '/' + split_flux1.name]
+    index_list = [influx.name + "/" + split_flux1.name]
     if split_flux2 is not None:
         split_flux2 = _prepare_input_fluxes(sampled_fluxes, split_flux2)
         mean_list.append(np.abs(np.mean(split_flux2 / influx)))
         stdev_list.append(np.std(split_flux2 / influx))
-        index_list.append(influx.name + '/' + split_flux2.name)
-    fluxes = {'Mean': mean_list,
-        'Stdev': stdev_list,
-        }    
-    output_df = pd.DataFrame(fluxes, columns = ['Mean','Stdev'], index = [index_list])
+        index_list.append(influx.name + "/" + split_flux2.name)
+    fluxes = {
+        "Mean": mean_list,
+        "Stdev": stdev_list,
+    }
+    output_df = pd.DataFrame(
+        fluxes, columns=["Mean", "Stdev"], index=[index_list]
+    )
     return output_df
 
 
@@ -417,21 +501,37 @@ def plot_split_ratio(
     if split_flux2 is not None:
         split_flux2 = _prepare_input_fluxes(sampled_fluxes, split_flux2)
         sum_ratios = abs(split_flux1 / influx) + abs(split_flux2 / influx)
-        split_df = pd.DataFrame({
-            'Sum': sum_ratios,
-            split_flux1.name: abs(split_flux1 / influx),
-            split_flux2.name: abs(split_flux2 / influx),
-        })
-        plot_title = f'{influx.name} / {split_flux1.name} vs.\n{influx.name} / {split_flux2.name}'
+        split_df = pd.DataFrame(
+            {
+                "Sum": sum_ratios,
+                split_flux1.name: abs(split_flux1 / influx),
+                split_flux2.name: abs(split_flux2 / influx),
+            }
+        )
+        plot_title = (
+            f'{influx.name} / {split_flux1.name} vs.\n'
+            f'{influx.name} / {split_flux2.name}'
+            )
     else:
-        split_df = pd.DataFrame({
-            split_flux1.name: abs(split_flux1 / influx),
-        })
-        plot_title = f'{influx.name} / {split_flux1.name}'
+        split_df = pd.DataFrame(
+            {
+                split_flux1.name: abs(split_flux1 / influx),
+            }
+        )
+        plot_title = f"{influx.name} / {split_flux1.name}"
+
     re_arranged_df = _reduce_sampled_fluxes(split_df, split_df.columns)
-    fig = sns.boxplot(x="Reaction", y="Sampled Fluxes", data=re_arranged_df, orient = 'v')
+
+    fig = sns.boxplot(
+        x="Reaction", y="Sampled Fluxes", data=re_arranged_df, orient="v"
+    )
     plt.xticks(rotation=70)
-    plt.title(f'{branch_point_name}:\n{plot_title}', size = 15)    """
+    plt.title(f"{branch_point_name}:\n{plot_title}", size=15)
+    return fig
+
+
+def get_observable_fluxes(fittedFluxes):
+    """
     Checks which calculated fluxes qualify as being 'observable'
     (flux value at least 4 times the confidence interval and 0
     not included in the confidence interval).
@@ -450,6 +550,17 @@ def plot_split_ratio(
         observable fluxes.
     """
     big_enough_fluxes = fittedFluxes[
+        fittedFluxes["flux"]
+        / (fittedFluxes["flux_ub"] - fittedFluxes["flux_lb"])
+        >= 4
+    ]
+    lb_below_0 = big_enough_fluxes["flux_lb"] < 0
+    ub_above_0 = 0 < big_enough_fluxes["flux_lb"]
+    observable_fluxes = big_enough_fluxes[~(lb_below_0 & ub_above_0)]
+    return observable_fluxes
+
+
+def percent_observable_fluxes(fittedFluxes, as_num=False):
     """
     Calculates the percentage of how many of the fluxes in
     the input dataframe qualify as observable fluxes.
@@ -467,6 +578,15 @@ def plot_split_ratio(
         relative to total number of fluxes.
     """
     percent_float = (
+        len(get_observable_fluxes(fittedFluxes)) / len(fittedFluxes)
+    ) * 100
+    if as_num:
+        return percent_float
+    else:
+        return print(round(percent_float, 2), "%")
+
+
+def get_flux_precision(fittedFluxes):
     """
     Calculates the mean standard deviation of the observable
     fluxes.
