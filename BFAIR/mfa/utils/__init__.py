0758ec193b90d184e6b33b4f0e55005bd410ed8d
mmattano <57752712+mmattano@users.noreply.github.com>
2021-05-19 12:22:11 +0200

    Update __init__.py

--- BFAIR/mfa/utils/__init__.py
+++ BFAIR/mfa/utils/__init__.py
@@ -1,12 +1,12 @@
 """Utils.
 Calculate MFA specific relevant parameters"""
 
-from BFAIR.mfa.sampling.flux_split import (
+from BFAIR.mfa.utils.flux_split import (
     calculate_split_ratio,
     plot_split_ratio,
 )
 
-from BFAIR.mfa.sampling.observable import (
+from BFAIR.mfa.utils.observable import (
     get_observable_fluxes,
     percent_observable_fluxes,
     get_flux_precision,
