6e420f3bbc3e19def9651d015d8509a2b45f618e
mmattano <57752712+mmattano@users.noreply.github.com>
2021-05-27 11:26:25 +0200

    Update flux_split.py

--- BFAIR/mfa/utils/flux_split.py
+++ BFAIR/mfa/utils/flux_split.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from BFAIR.mfa.sampling.visualization.distributions import (
+from BFAIR.mfa.visualization.distributions import (
     _reduce_sampled_fluxes
 )
 
