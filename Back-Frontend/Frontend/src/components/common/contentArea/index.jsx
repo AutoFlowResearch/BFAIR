7c1224323c3567d81d553f9c17eb4bc90e0e2c65
Lawrence <lawegyir@yahoo.com>
2021-05-29 20:13:07 +0530

    added new css and change layout
    added new css and change layout

--- Back-Frontend/Frontend/src/components/common/contentArea/index.jsx
+++ Back-Frontend/Frontend/src/components/common/contentArea/index.jsx
@@ -1,171 +1,155 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-class ContentArea extends Component {
-  state = {
-    role: 'admin',
-  };
+export class Mainview extends Component {
+    constructor(props) {
+        super(props)
 
-  componentDidMount() {
-    // console.log(this.props.location);
-    // this.props.history.push(`/mainpage/${this.state.role}/dashboard`);
-  }
+        this.state = {
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-lg-12 col-md-12 col-sm-12">
-            <div class="d-flex justify-content-between">
-              <b>Module</b>
-              <div class="main">
-                <div class="form-group has-search">
-                  <span
-                    id="searchSpan"
-                    class="fa fa-search form-control-feedback"
-                  ></span>
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Search for module name"
-                  />
+        }
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                <div className="row mt-4">
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <div class="d-flex justify-content-between">
+                            <b>Module</b>
+                            <div class="main">
+                                <div class="form-group has-search">
+                                    <span class="fa fa-search form-control-feedback"></span>
+                                    <input type="text" class="form-control" placeholder="Search for module name" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <div className="card Mainview">
-              <div className="card-body">
                 <div className="row">
-                  <div className="col-lg-8 col-md-8 col-sm-8">
-                    <div className="innerText">
-                      <b>DOE</b>
-                      <p>
-                        It has survived not only five centuries, but also the
-                        leap into electronic
-                      </p>
-                      <a href="bootstrap_flex.asp" className="readmore">
-                        Read More<i class="fas fa-arrow-right"></i>
-                      </a>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="card Mainview">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-8 col-md-8 col-sm-8">
+                                        <div className="innerText">
+                                            <b>DOE</b>
+                                            <p>
+                                                It has survived not only five centuries,
+                                                but also the leap into electronic
+                                                
+                                            </p>
+                                            <a href="bootstrap_flex.asp" className="readmore">Read More<i class="fas fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-4 col-sm-4">
+                                        <div className="innerBox">
+                                            <b>DOE</b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4 col-sm-4">
-                    <div className="innerBox">
-                      <b>DOE</b>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="card Mainview">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-8 col-md-8 col-sm-8">
+                                        <div className="innerText">
+                                            <b>Result Retrival</b>
+                                            <p>
+                                                It has survived not only five centuries,
+                                                but also the leap into electronic
+                                            </p>
+                                            <a href="bootstrap_flex.asp" className="readmore">Read More<i class="fas fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-4 col-sm-4">
+                                        <div className="innerBox">
+                                            <b>Result Retrival</b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <div className="card Mainview">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-lg-8 col-md-8 col-sm-8">
-                    <div className="innerText">
-                      <b>Result Retrival</b>
-                      <p>
-                        It has survived not only five centuries, but also the
-                        leap into electronic
-                      </p>
-                      <a href="bootstrap_flex.asp" className="readmore">
-                        Read More<i class="fas fa-arrow-right"></i>
-                      </a>
+                <div className="row pt-3">
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="card Mainview">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-8 col-md-8 col-sm-8">
+                                        <div className="innerText">
+                                            <b>Module 3</b>
+                                            <p>
+                                                It has survived not only five centuries,
+                                                but also the leap into electronic
+                                            </p>
+                                            <a href="bootstrap_flex.asp" className="readmore">Read More<i class="fas fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-4 col-sm-4">
+                                        <div className="innerBox">
+                                        <b>Module 3</b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4 col-sm-4">
-                    <div className="innerBox">
-                      <b>Result Retrival</b>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <div className="card Mainview">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-8col-md-8 col-sm-8">
+                                        <div className="innerText">
+                                            <b>Module 4</b>
+                                            <p>
+                                                It has survived not only five centuries,
+                                                but also the leap into electronic
+                                            </p>
+                                            <a href="bootstrap_flex.asp" className="readmore">Read More<i class="fas fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4 col-md-4 col-sm-4">
+                                        <div className="innerBox">
+                                        <b>Module 4</b>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row pt-3">
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <div className="card Mainview">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-lg-8 col-md-8 col-sm-8">
-                    <div className="innerText">
-                      <b>Module 3</b>
-                      <p>
-                        It has survived not only five centuries, but also the
-                        leap into electronic
-                      </p>
-                      <a href="bootstrap_flex.asp" className="readmore">
-                        Read More<i class="fas fa-arrow-right"></i>
-                      </a>
+                <div className="row mt-3">
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                        <h5>Project Source Code</h5>
+                        <div className="card bottomCard">
+                            <div className="card-body">
+                                <div className="row">
+                                    <div className="col-lg-2 col-md-2 col-sm-2">
+                                        <div className="innerBox"><i class="fa innerFile fa-file-code"></i></div>
+                                    </div>
+                                    <div className="col-lg-10 col-md-10 col-sm-10">
+                                        <div className="innerText">
+                                            <b>Source code</b>
+                                            <p>
+                                                It has survived not only five centuries,
+                                                but also the leap into electronic
+                                            </p>
+                                            <a href="bootstrap_flex.asp" className="readmore">Read More<i class="fas fa-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4 col-sm-4">
-                    <div className="innerBox">
-                      <b>Module 3</b>
-                    </div>
-                  </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <div className="card Mainview">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-lg-8col-md-8 col-sm-8">
-                    <div className="innerText">
-                      <b>Module 4</b>
-                      <p>
-                        It has survived not only five centuries, but also the
-                        leap into electronic
-                      </p>
-                      <a href="bootstrap_flex.asp" className="readmore">
-                        Read More<i class="fas fa-arrow-right"></i>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="col-lg-4 col-md-4 col-sm-4">
-                    <div className="innerBox">
-                      <b>Module 4</b>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mt-3">
-          <div className="col-lg-12 col-md-12 col-sm-12">
-            <h5>Project Source Code</h5>
-            <div className="card bottomCard">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-lg-2 col-md-2 col-sm-2">
-                    <div className="innerBox">Lawrence</div>
-                  </div>
-                  <div className="col-lg-10 col-md-10 col-sm-10">
-                    <div className="innerText">
-                      <b>Source code</b>
-                      <p>
-                        It has survived not only five centuries, but also the
-                        leap into electronic
-                      </p>
-                      <a href="bootstrap_flex.asp" className="readmore">
-                        Read More<i class="fas fa-arrow-right"></i>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+        )
+    }
 }
 
-export default ContentArea;
+export default Mainview
--- Back-Frontend/Frontend/src/components/common/sidebarArea/index.jsx
+++ Back-Frontend/Frontend/src/components/common/sidebarArea/index.jsx
@@ -1,115 +1,60 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { logout } from '../../../store/actions/auth';
-import user from '../../../../node_modules/admin-lte/dist/img/user2-160x160.jpg';
 
-class SidebarArea extends Component {
-  // handleLogout = () => {
-  //   const { dispatch } = this.props;
-  //   dispatch(logout());
-  //   window.location = "/";
-  // };
+import React, { Component } from 'react'
 
-  render() {
-    // const { pathname } = this.props.location;
-    // const currentTab = pathname.split("/").pop();
-    return (
-      <div className="sideBar">
-        <nav class="navbar">
-          <div className="brand">{/* <h5 className="pl-3">BFAIR</h5>  */}</div>
+export class LeftSidebar extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+<div className="sideBar"> 
+      
+<nav class="navbar">
+<ul className="navbar-nav pt-3">
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-file-code leftSidebarIcon"></i>Dashboard</a>
+  </li>
+  <li className="nav-item">
+    <a class="nav-link" href="#"><i class="fa fa-file-code leftSidebarIcon"></i>Source code</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-lightbulb leftSidebarIcon"></i>Ideas</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-users leftSidebarIcon"></i>Contacts</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-user leftSidebarIcon"></i>Agents</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-newspaper leftSidebarIcon"></i>Articles</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-cog leftSidebarIcon"></i>Settings</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#"><i class="fa fa-ring leftSidebarIcon"></i>Subscriptions</a>
+  </li>
+</ul>
 
-          <ul className="navbar-nav pt-3">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Dashboard
-              </a>
-            </li>
-            <li className="nav-item">
-              <a class="nav-link" href="#">
-                <i class="far fa-file-code"></i>Source code
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Ideas
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Contacts
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Agents
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Articles
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Settings
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <i class="far fa-file-code"></i>Subscriptions
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="sidebarBox">
-          <div className="sideboxContent">
-            <span>Need Help?</span>
-            <p>Please Check our docs</p>
-            <button type="button" className="btn leftBtn">
-              Documentation
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+</nav>
+<div className="sidebarBox">
+<span><i class="fa fa-file-alt sidebarboxIcon"></i></span>
+  <div className="sideboxContent">
+  
+  <span>Need Help?</span>
+  <p>Please Check our docs</p>
+<button type="button" className="btn leftBtn">Documentation</button>                                          
+</div>
+</div>
+</div>    
+        )
+    }
 }
 
-export default connect()(SidebarArea);
-
-{
-  /* <div classNameName="col-sm-3 sidenav">
-  <div classNameName="list-group mt-2">
-    <NavLink
-      to="/mainpage/admin/dashboard"
-      classNameName={`list-group-item ${
-        currentTab === "dashboard" ? "active" : ""
-        }`}
-    >
-      Dashboard
-          </NavLink>
-
-    <NavLink
-      to="/mainpage/admin/user-management"
-      classNameName={`list-group-item ${
-        currentTab === "user-management" ? "active" : ""
-        }`}
-    >
-      User Management
-          </NavLink>
-
-    <NavLink
-      to="/mainpage/admin/posts"
-      classNameName={`list-group-item ${
-        currentTab === "posts" ? "active" : ""
-        }`}
-    >
-      Posts
-          </NavLink>
-    <span classNameName="list-group-item" onClick={this.handleLogout}>
-      <button classNameName="btn btn-danger">Logout</button>
-    </span>
-  </div>
-</div> */
-}
+export default LeftSidebar
